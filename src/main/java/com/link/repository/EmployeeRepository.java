7b6095bd9daa548925bb079c24365e71623f57a9
fengqingyebai <fengqingyebai@126.com>
2018-04-09 07:39:31 +0800

    springdatajpa测试

--- src/main/java/com/link/repository/EmployeeRepository.java
+++ src/main/java/com/link/repository/EmployeeRepository.java
@@ -2,7 +2,11 @@ package com.link.repository;
 
 
 import com.link.domain.Employee;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.RepositoryDefinition;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * 员工的数据访问组件接口
@@ -10,10 +14,41 @@ import org.springframework.data.repository.Repository;
  * @author link
  * @create 2018-04-08-23:04
  */
-public interface EmployeeRepository  extends Repository<Employee,Integer> {
+@RepositoryDefinition(domainClass = Employee.class,idClass = Integer.class)
+public interface EmployeeRepository  {//extends Repository<Employee,Integer> {
 
 
   public Employee findByName(String name);
+//where name like ?% and age<?
+  public List<Employee> findByNameStartingWithAndAgeLessThan(String name,Integer age);
+  //where name like %? and age<?
+  public List<Employee> findByNameEndingWithAndAgeLessThan(String name,Integer age);
 
+  //where name in (?,？，？..) or age<?
+  public List<Employee> findByNameInOrAgeLessThan(List<String> names,Integer age);
+
+  //where name in (?,？，？..) and age<?
+  public List<Employee> findByNameInAndAgeLessThan(List<String> names,Integer age);
+
+  @Query("select o from Employee o where o.id=(select count(o.id) from Employee o )")
+  public Employee getEmployeeMaxId();
+
+  @Query("select o from Employee o where o.age=?1 and o.name=?2")
+  public List<Employee>  queryParams1(Integer age,String name);
+
+
+  @Query("select o from Employee o where o.age=:age and o.name=:name")
+  public List<Employee>  queryParams2(@Param("age") Integer age, @Param("name") String name);
+
+  @Query("select o from Employee o where o.name like %?1%")
+  public List<Employee>  queryLike1(String name);
+
+
+  @Query("select o from Employee o where o.name like %:name%")
+  public List<Employee>  queryLike2(@Param("name") String name);
+
+
+  @Query(nativeQuery =true,value = "select count(1) from employee")
+  public Long getCount();
 
 }
--- src/test/java/com/link/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/link/repository/EmployeeRepositoryTest.java
@@ -8,6 +8,9 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * springdata测试
  *
@@ -30,11 +33,130 @@ public class EmployeeRepositoryTest {
      * 测试EntityManagerFactory
      */
     @Test
-    public void testEmployeeRepository() {
+    public void testFindByName() {
         Employee employee = employeeRepository.findByName("zhangsan");
 
         System.out.println("id: " + employee.getId() + " ,name: " + employee.getName() + " ,age: " + employee.getAge());
     }
+
+
+    /**
+     * 测试EntityManagerFactory
+     */
+    @Test
+    public void testFindByNameStartingWithAndAgeLessThan() {
+        List<Employee> employees = employeeRepository.findByNameStartingWithAndAgeLessThan("test",25);
+        for (Employee employee:employees) {
+            System.out.println("id: " + employee.getId() + " ,name: " + employee.getName() + " ,age: " + employee.getAge());
+        }
+    }
+
+    /**
+     * 测试EntityManagerFactory
+     */
+    @Test
+    public void testFindByNameEndingWithAndAgeLessThan() {
+        List<Employee> employees = employeeRepository.findByNameEndingWithAndAgeLessThan("6",22);
+        for (Employee employee:employees) {
+            System.out.println("id: " + employee.getId() + " ,name: " + employee.getName() + " ,age: " + employee.getAge());
+        }
+    }
+
+
+    @Test
+    public void testFindByNameInOrAgeLessThan() {
+
+        List<String> names = new ArrayList<String>();
+        names.add("test1");
+        names.add("test2");
+        names.add("test3");
+
+        List<Employee> employees = employeeRepository.findByNameInOrAgeLessThan(names,22);
+        for (Employee employee:employees) {
+            System.out.println("id: " + employee.getId() + " ,name: " + employee.getName() + " ,age: " + employee.getAge());
+        }
+    }
+
+    @Test
+    public void testFindByNameInAndAgeLessThan() {
+
+        List<String> names = new ArrayList<String>();
+        names.add("test1");
+        names.add("test2");
+        names.add("test3");
+
+        List<Employee> employees = employeeRepository.findByNameInAndAgeLessThan(names,22);
+        for (Employee employee:employees) {
+            System.out.println("id: " + employee.getId() + " ,name: " + employee.getName() + " ,age: " + employee.getAge());
+        }
+    }
+
+
+
+
+    @Test
+    public void testGetEmployeeMaxId() {
+
+        List<String> names = new ArrayList<String>();
+        names.add("test1");
+        names.add("test2");
+        names.add("test3");
+
+        Employee employee = employeeRepository.getEmployeeMaxId();
+
+            System.out.println("id: " + employee.getId() + " ,name: " + employee.getName() + " ,age: " + employee.getAge());
+    }
+
+    @Test
+    public void testQueryParams1() {
+
+        List<Employee>  employees = employeeRepository.queryParams1(22,"test2");
+        for (Employee employee:employees) {
+            System.out.println("id: " + employee.getId() + " ,name: " + employee.getName() + " ,age: " + employee.getAge());
+        }
+    }
+
+
+    @Test
+    public void testQueryParams2() {
+
+        List<Employee>  employees = employeeRepository.queryParams2(22,"test2");
+        for (Employee employee:employees) {
+            System.out.println("id: " + employee.getId() + " ,name: " + employee.getName() + " ,age: " + employee.getAge());
+        }
+    }
+
+
+
+    @Test
+    public void testQueryLike1() {
+
+        List<Employee>  employees = employeeRepository.queryLike1("test1");
+        for (Employee employee:employees) {
+            System.out.println("id: " + employee.getId() + " ,name: " + employee.getName() + " ,age: " + employee.getAge());
+        }
+    }
+
+
+    @Test
+    public void testQueryLike2() {
+
+        List<Employee>  employees = employeeRepository.queryLike2("test1");
+        for (Employee employee:employees) {
+            System.out.println("id: " + employee.getId() + " ,name: " + employee.getName() + " ,age: " + employee.getAge());
+        }
+    }
+
+
+
+    @Test
+    public void testGetCount() {
+        long count = employeeRepository.getCount();
+        System.out.println("count:"+count);
+    }
+
+
+
 
     @After
     public void tearContext() {
